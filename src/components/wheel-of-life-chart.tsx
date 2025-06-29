fef45e68856fdcf3d2d4a30118ff536ee6b103b9
Amin BADH <aminbadh@gmail.com>
2025-06-29 16:59:46 +0000

    undo the most recent changes

--- src/components/wheel-of-life-chart.tsx
+++ src/components/wheel-of-life-chart.tsx
@@ -53,10 +53,10 @@ export default function WheelOfLifeChart({ scores, categories }: WheelOfLifeChar
 
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <RadarChart cx="50%" cy="50%" outerRadius="85%" data={chartData}>
+            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={chartData}>
                 <PolarGrid stroke={borderColor} />
                 <PolarAngleAxis dataKey="subject" tick={renderIconTick} />
-                <PolarRadiusAxis angle={30} domain={[0, 12]} tick={false} axisLine={false} />
+                <PolarRadiusAxis angle={30} domain={[0, 10]} tick={false} axisLine={false} />
                 <Radar
                     name="Life Compass"
                     dataKey="value"
